754202b4bb8a6bcd793d2e647955444471be380e
alyssa reyes <alyssa.va.reyes@gmail.com>
2021-05-17 09:17:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let searchButton = document.querySelector("#search");
+
+
+//Add an event listener to the button that runs the function sendApiRequest when it is clicked
+searchButton.addEventListener("click", ()=>{
+    console.log("button pressed");
+    sendApiRequest()
+})
+
+
+//An asynchronous function to fetch data from the API
+async function sendApiRequest(){ //this function is calling to request data from API, it runs when the button is pressed
+    let API_KEY = "Mjtf2IZyDfFJleCpNwqKhsaMKlxhswWHB13c0qsV"
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`); //interpilated api key.. then made a variable that waits for the fetch result to go out and come back.
+    console.log(response);
+    let data = await response.json(); //data retrieval, without "await", we are making a promise that the data is coming back. With "await", the data will come back
+    console.log(data);
+    useApiData(data) //now that the data is retrieved, we want to do something with it- we are passing the data to function
+}
+
+
+//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with 
+function useApiData(data){
+    document.querySelector("#caption").innerHTML = data.explanation //retreiving a specific piece of data from API, in this case it is the explanation
+    document.querySelector("#content").innerHTML = `<img src="${data.url}">` 
+    
+}
+
+
